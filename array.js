7a1ee5e3f2a01c8a6fd2f0cda37d4e28a85157b4
Pavel Parkhomey <pavelpark@aol.com>
2016-10-26 11:56:26 -0700

    got the functions going

---
+++ array.js
@@ -0,0 +1,14 @@
+var portfolioArticles = [
+  {
+    title: 'About me',
+    paragraph: 'Pavel Parkhomey'
+  },
+  {
+    title: 'Projects',
+    paragraph: 'project01'
+  },
+  {
+    title: 'Resume',
+    paragraph: '<p>Bacon ipsum dolor amet do ex andouille minim, kielbasa consectetur t-bone. Ullamco alcatra deserunt, occaecat sed drumstick prosciutto. Cupim sed ullamco cow ham hock turkey pariatur. Officia landjaeger minim, tenderloin salami ribeye cupidatat. Consequat reprehenderit shank nostrud proident shoulder mollit flank. Excepteur quis occaecat kevin officia, ribeye sausage chuck et pig.</p><p><img src="http://baconmockup.com/600/300/" class="pull-right" > Cupidatat pancetta chuck fugiat shankle dolor drumstick excepteur spare ribs duis. Meatloaf beef ribs et picanha eu in qui dolore tongue enim spare ribs capicola est pork chop swine. Ipsum strip steak nostrud ham hock qui irure. <img src="http://baconmockup.com/300/400/" class="pull-right">Picanha occaecat prosciutto, rump tempor jerky proident voluptate short loin jowl.</p>'
+  }
+];
